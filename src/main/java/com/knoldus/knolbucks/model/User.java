629bf31d7de81b7eafe31bce2c1ad04956094fa9
mkapoor <meghna.kapoor@knoldus.in>
2019-05-15 13:33:54 +0530

    updated User to include lombok

---
+++ src/main/java/com/knoldus/knolbucks/model/User.java
@@ -0,0 +1,30 @@
+package com.knoldus.knolbucks.model;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+/**
+ * 
+ * @author meghna
+ *
+ */
+@Getter @Setter @NoArgsConstructor
+public class User {
+	
+	private Integer id;
+	
+	private String name;
+	
+	private String email;
+	
+	private Date dateOfBirth;
+	
+	private RoleEnum role;
+	
+	private Boolean active;
+
+	private String employeeId;
+	
+}
